d2bfa2aa841c52eec79b57ef36700ca3e5dc58c9
Sanjana Kumari <sanjanasanjeevcisf@gmail.com>
2026-04-01 14:55:04 +0530

    final fix for reset endpoint

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from server.app import app
+def main():
+    import uvicorn
+    uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=7860)
+    main()
